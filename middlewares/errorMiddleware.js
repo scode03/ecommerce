b0b6ec66deee0ed57a47a69e127e67b8d8fb9ffc
scode03 <sofyan.workspace@gmail.com>
2025-05-27 19:32:02 +0700

    conf errorhandling url

---
+++ middlewares/errorMiddleware.js
@@ -0,0 +1,16 @@
+export const notFound = (req, res, next) => {
+  const error = new Error(`path not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+export const errorHandler = (err, req, res, next) => {
+  let resStatusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  let message = err.message;
+
+  res.status(resStatusCode).json({
+    message,
+    stack: err.stack,
+  });
+};
--- server/app.js
+++ server/app.js
@@ -1,25 +1,25 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-import authRouter from './routes/authRouter.js'
+import { notFound, errorHandler } from "../middlewares/errorMiddleware.js";
+import authRouter from "./routes/authRouter.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // Parent Router
-app.use('/api/v1/auth', authRouter)
+app.use("/api/v1/auth", authRouter);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
-})
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
 // Connection DB
-mongoose.connect(process.env.DATABASE, {
-
-}).then(() => {
-  console.log("Database Connected")
-})
-
+mongoose.connect(process.env.DATABASE, {}).then(() => {
+  console.log("Database Connected");
+});
