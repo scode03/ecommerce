c68dc6121d6d6866bfc419435b849adb7328e674
scode03 <sofyan.workspace@gmail.com>
2025-06-28 18:59:17 +0700

    Finish endpoint update and delete product

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -5,7 +5,7 @@ export const createProduct = asyncHandler(async (req, res) => {
   const newProduct = await Product.create(req.body);
 
   res.status(201).json({
-    message: "add product succesful",
+    message: "Add product succesful",
     data: newProduct,
   });
 });
@@ -35,11 +35,25 @@ export const detailProduct = asyncHandler(async (req, res) => {
 });
 
 export const updateProduct = asyncHandler(async (req, res) => {
-  res.send("Update Product");
+  const paramId = req.params.id;
+  const updateProduct = await Product.findByIdAndUpdate(paramId, req.body, {
+    runValidators: false,
+    new: true,
+  });
+
+  return res.status(201).json({
+    message: "Update product succesful",
+    data: updateProduct,
+  });
 });
 
 export const deleteProduct = asyncHandler(async (req, res) => {
-  res.send("Delete Product");
+  const paramId = req.params.id;
+  await Product.findByIdAndDelete(paramId);
+
+  return res.status(200).json({
+    message: "Delete product succesfull",
+  });
 });
 
 export const fileUpload = asyncHandler(async (req, res) => {
